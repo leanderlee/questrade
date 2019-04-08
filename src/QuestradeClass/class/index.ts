df2a2aa0c2fba13703f2a10f51dc9bd1fc1813b8
Benjamin Vincent <luxcium@neb401.com>
2019-04-08 08:33:26 -0400

    swiched code refactored

--- src/QuestradeClass/class/index.ts
+++ src/QuestradeClass/class/index.ts
@@ -1,14 +1,13 @@
 /** @format */
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+import * as axios from 'axios';
 import { EventEmitter as EE } from 'events';
-import {
-  /* readFile, */ readFileSync,
-  /*  writeFile, */ writeFileSync,
-} from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import { chain, keyBy, pick } from 'lodash';
 import { sync } from 'mkdirp';
 import { default as moment } from 'moment';
 import { dirname } from 'path';
-import { default as request } from 'request-promise-native';
+import { ICreds } from './auth/ICreds';
 // import { promisify } from 'util';
 type seedToken = string;
 type keyFile = string;
@@ -664,138 +663,440 @@ export class QuestradeClass extends EE {
     }
   };
 
+  //   private saveKey = async () => {
+  //     // !!!
+  //     try {
+  //       if (introspection) console.log('\n', '_saveKey = async () => {');
+  //       try {
+  //         this._writeFileSync(this._getKeyFile(), this.
+  //         _refreshToken, 'utf8');
+  //       } catch (error) {
+  //         throw new Error('failed_to_write');
+  //       }
+  //       return this._refreshToken;
+  //     } catch (error) {
+  //       if (introspection) console.log('\n', error.message);
+  //       throw error;
+  //     }
+  //   };
+
+  //   // Gets name of the file where the refreshToken is stored
+  //   private getKeyFile = () => {
+  //     // !!!
+  //     try {
+  //       if (introspection) console.log('\n', '_getKeyFile = () => {');
+  //       return this._keyFile || `${this._keyDir}/${this.seedToken}`;
+  //     } catch (error) {
+  //       if (introspection) console.log('\n', error.message);
+  //       throw error;
+  //     }
+  //   };
+
+  //   // Reads the refreshToken stored in the file (if it exist)
+  //   // otherwise uses the seedToken
+  //   private loadKey = () => {
+  //     // !!!
+  //     try {
+  //       if (introspection) console.log('\n', '_loadKey = async () => {');
+  //       if (this._keyFile) {
+  //         sync(dirname(this._keyFile));
+  //       } else {
+  //         sync(this._keyDir);
+  //       }
+
+  //       if (introspection) console.log('\n', 'will read');
+  //       const refreshToken: any =
+  //               this._readFileSync(this._getKeyFile(), 'utf8');
+  //       if (introspection) console.log('\n', 'did read');
+  //       if (!refreshToken) {
+  //         this._refreshToken = this.seedToken;
+  //         this._saveKey();
+  //       }
+  //       this._refreshToken = refreshToken;
+  //       return refreshToken;
+  //     } catch (error) {
+  //       if (introspection) console.log('\n', 'ERROR: _loadKey = () => {');
+  //       if (introspection) console.log('\n', error.message);
+  //       // throw error;
+  //     }
+  //   };
+
+  //   // Refreshed the tokem (aka Logs in) using the latest RefreshToken
+  //   // (or the SeedToken if no previous saved file)
+  //   private refreshKey = async () => {
+  //     // !!!
+  //     try {
+  //       if (introspection) console.log('\n', '_refreshKey = async () => {');
+  //       const data = {
+  //         grant_type: 'refresh_token',
+  //         refresh_token: this._refreshToken,
+  //       };
+  //       const res = await request({
+  //         method: 'POST',
+  //         qs: data,
+  //         url: `${this._authUrl}/oauth2/token`,
+  //       });
+  //       const creds = await JSON.parse(res.body);
+  //       if (introspection) console.log('\n', 'creds');
+  //       if (introspection) console.dir(creds);
+  //       if (introspection) console.log('\n', 'creds.api_server');
+  //       if (introspection) console.dir(creds.api_server);
+  //       this._apiServer = await creds.api_server;
+  //       this._apiUrl = (await creds.api_server) + this._apiVersion;
+  //       this._accessToken = await creds.access_token;
+  //       this._refreshToken = await creds.refresh_token;
+  //       await this._saveKey();
+  //       this.emit('refresh', this._refreshToken);
+  //     } catch (error) {
+  //       if (introspection) console.log('\n', error.message);
+  //       // throw error;
+  //     }
+  //   };
+
+  //   // Method that actually mades the GET/POST request to Questrade
+  //   private api = async (
+  //     method?: string,
+  //     endpoint?: string | number,
+  //     params?: any
+  //   ) => {
+  //     // !!!
+  //     try {
+  //       if (introspection) console.log('\n', '_api = async (...) => {');
+  //       const url: string = this._apiUrl + endpoint;
+  //       const opts: any = {
+  //         auth: {
+  //           bearer: this._accessToken,
+  //         },
+  //         method,
+  //         url,
+  //       };
+  //       if (method === 'GET') {
+  //         opts.qs = params || {};
+  //         opts.json = true;
+  //       } else {
+  //         opts.json = params || true;
+  //       }
+  //       return request(opts);
+  //     } catch (error) {
+  //       if (introspection) console.log('\n', error.message);
+  //       // throw error;
+  //     }
+  //   };
+
+  //   // Method that appends the set account to the API calls so all calls
+  //   // are made to that account. Chage this.
+  // account to change the account used
+  //   private accountApi = async
+  // (method?: any, endpoint?: any, params?: any) => {
+  //     // !!!
+  //     try {
+  //       if (introspection) console.log('\n', '_accountApi = (...) => {');
+  //       if (!this._account) {
+  //         throw new Error('no_account_selected');
+  //       }
+  // return this._api(method, `/accounts/${this._account}${endpoint}`, params);
+  //     } catch (error) {
+  //       if (introspection) console.log('\n', error.message);
+  //       // throw error;
+  //     }
+  //   };
+  // private getUnused(){
+  //    this._saveKey();
+  //      this._getKeyFile();
+  //      this._loadKey();
+  //      this._refreshKey();
+  //      this._api();
+  //      this._accountApi();
+  // }
+  // Running the Authentification process and emit 'ready' when done
+  // function constructor(){
+  // !!  self._loadKey(function(err) {
+  //     if (err)
+  //       return self.emit('error', {
+  //         message: 'failed_to_load_key',
+  //         details: err,
+  //       });
+  // !!    self._refreshKey(function(err) {
+  //       if (err)
+  //         return self.emit('error', {
+  //           message: 'failed_to_refresh_key',
+  //           details: err,
+  //         });
+  //       if (self.account) return self.emit('ready');
+  // !!      self.setPrimaryAccount(function(err) {
+  //         if (err)
+  //           return self.emit('error', {
+  //             message: 'failed_to_set_account',
+  //             details: err,
+  //           });
+  //         self.emit('ready');
+  //       });
+  //     });
+  //   });
+  // }
+
+  // const this._account = '';
+  // const this._apiVersion = '';
+  // const this._authUrl = 'https://login.questrade.com';
+  // const this._keyDir = './keys';
+  // const this._keyFile = '';
+  // const seedToken = 'FADh7OnO_gBUXkeg3WLhZGKosAac6IbF0';
+
+  // let this._accessToken = '';
+  // let this._apiServer = '';
+  // let this._apiUrl = '';
+  // let this._refreshToken = '';
+
+  /*
+Saves the latest refreshToken in the file name after the seedToken
+Questrade.prototype._saveKey = function(cb) {
+  cb = cb || function() {};
+  var self = this;
+  fs.writeFile(self._getKeyFile(), self.refreshToken, 'utf8', function(err) {
+    if (err)
+      return cb({
+        message: 'failed_to_write',
+        details: err,
+      });
+    cb(null, self.refreshToken);
+  });
+};
+*/
   private _saveKey = async () => {
-    // !!!
-    try {
-      if (introspection) console.log('\n', '_saveKey = async () => {');
-      try {
-        this._writeFileSync(this._getKeyFile(), this._refreshToken, 'utf8');
-      } catch (error) {
-        throw new Error('failed_to_write');
-      }
-      return this._refreshToken;
-    } catch (error) {
-      if (introspection) console.log('\n', error.message);
-      throw error;
-    }
+    writeFileSync(this._getKeyFile(), this._refreshToken, 'utf8');
+    return this._refreshToken;
   };
 
-  // Gets name of the file where the refreshToken is stored
+  /*
+Gets name of the file where the refreshToken is stored
+Questrade.prototype._getKeyFile = function() {
+  return this.keyFile || this.keyDir + '/' + this.seedToken;
+};
+
+*/
   private _getKeyFile = () => {
-    // !!!
-    try {
-      if (introspection) console.log('\n', '_getKeyFile = () => {');
-      return this._keyFile || `${this._keyDir}/${this.seedToken}`;
-    } catch (error) {
-      if (introspection) console.log('\n', error.message);
-      if (introspection) console.log('\n', 'ERROR : _getKeyFile = () => {');
-      throw error;
-    }
+    return this._keyFile || `${this._keyDir}/${this.seedToken}`;
   };
-
-  // Reads the refreshToken stored in the file (if it exist)
-  // otherwise uses the seedToken
+  /*
+Reads the refreshToken stored in the file (if it exist),
+otherwise uses the seedToken
+Questrade.prototype._loadKey = function(cb) {
+  cb = cb || function() {};
+  var self = this;
+  if (self.keyFile) {
+    mkdirp.sync(path.dirname(self.keyFile));
+    Synchronously create a new directory
+  } else {
+    mkdirp.sync(self.keyDir);
+  }
+  fs.readFile(self._getKeyFile(), 'utf8', function(err, refreshToken) {
+    if (err || !refreshToken) {
+      self.refreshToken = self.seedToken;
+      return self._saveKey(cb);
+    }
+    self.refreshToken = refreshToken;
+    cb(null, refreshToken);
+  });
+};
+*/
   private _loadKey = () => {
-    // !!!
-    try {
-      if (introspection) console.log('\n', '_loadKey = async () => {');
-      if (this._keyFile) {
-        sync(dirname(this._keyFile)); // Synchronously create a new directory
-      } else {
-        sync(this._keyDir);
-      }
-
-      if (introspection) console.log('\n', 'will read');
-      const refreshToken: any = this._readFileSync(this._getKeyFile(), 'utf8');
-      if (introspection) console.log('\n', 'did read');
-      if (!refreshToken) {
-        this._refreshToken = this.seedToken;
-        this._saveKey();
-      }
-      this._refreshToken = refreshToken;
-      return refreshToken;
-    } catch (error) {
-      if (introspection) console.log('\n', 'ERROR: _loadKey = () => {');
-      if (introspection) console.log('\n', error.message);
-      // throw error;
+    if (this._keyFile) {
+      sync(dirname(this._keyFile));
+    } else {
+      sync(this._keyDir);
     }
+    const refreshToken: any = readFileSync(this._getKeyFile(), 'utf8');
+    if (!refreshToken) {
+      this._refreshToken = this.seedToken;
+      this._saveKey();
+    }
+    this._refreshToken = refreshToken;
+    return refreshToken;
   };
-
-  // Refreshed the tokem (aka Logs in) using the latest RefreshToken
-  // (or the SeedToken if no previous saved file)
+  /*
+Refreshed the tokem (aka Logs in) using the latest RefreshToken
+(or the SeedToken if no previous saved file)
+Questrade.prototype._refreshKey = function(cb) {
+  var self = this;
+  var data = {
+    grant_type: 'refresh_token',
+    refresh_token: self.refreshToken,
+  };
+  request(
+    {
+      method: 'POST',
+      url: self.authUrl + '/oauth2/token',
+      qs: data,
+      data: data,
+    },
+    function(err, http, body) {
+      try {
+        var creds = JSON.parse(body);
+        self.api_server = creds.api_server;
+        self.apiUrl = creds.api_server + self.apiVersion;
+        self.accessToken = creds.access_token;
+        self.refreshToken = creds.refresh_token;
+        self._saveKey();
+        self.emit('refresh', self.refreshToken);
+      } catch (e) {
+        return cb({
+          message: 'login_failed',
+          token: self.refreshToken,
+          details: body,
+        });
+      }
+      cb();
+    }
+  );
+};
+*/
   private _refreshKey = async () => {
-    // !!!
+    let response: axios.AxiosResponse = {
+      data: null,
+      status: 0,
+      statusText: '',
+      headers: { Null: null },
+      config: {},
+    };
     try {
-      if (introspection) console.log('\n', '_refreshKey = async () => {');
-      const data = {
+      const url = `${this._authUrl}/oauth2/token`;
+      const params = {
         grant_type: 'refresh_token',
         refresh_token: this._refreshToken,
       };
-      const res = await request({
+      const axiosConfig: axios.AxiosRequestConfig = {
         method: 'POST',
-        qs: data,
-        url: `${this._authUrl}/oauth2/token`,
-      });
-      const creds = await JSON.parse(res.body);
-      if (introspection) console.log('\n', 'creds');
-      if (introspection) console.dir(creds);
-      if (introspection) console.log('\n', 'creds.api_server');
-      if (introspection) console.dir(creds.api_server);
-      this._apiServer = await creds.api_server;
-      this._apiUrl = (await creds.api_server) + this._apiVersion;
-      this._accessToken = await creds.access_token;
-      this._refreshToken = await creds.refresh_token;
+        params,
+        url,
+      };
+      response = await axios.default(axiosConfig);
+
+      const creds: ICreds = response.data;
+      this._apiServer = creds.api_server;
+      console.log('creds.api_server:', creds.api_server);
+      this._apiUrl = `${this._apiServer}${this._apiVersion}`;
+      this._accessToken = creds.access_token;
+      console.log('creds.access_token:', creds.access_token);
+      this._refreshToken = creds.refresh_token;
+      console.log('creds.refresh_token:', creds.refresh_token);
       await this._saveKey();
-      this.emit('refresh', this._refreshToken);
     } catch (error) {
-      if (introspection) console.log('\n', error.message);
-      // throw error;
+      console.log('response:', response);
+      console.error('ERROR:', error.message);
     }
   };
-
-  // Method that actually mades the GET/POST request to Questrade
+  /*
+Method that actually mades the GET/POST request to Questrade
+Questrade.prototype._api = function(method, endpoint, params, cb) {
+  cb = cb || function() {};
+  var self = this;
+  if (typeof params === 'function') {
+    cb = params;
+    params = undefined;
+  }
+  var opts = {
+    method: method,
+    url: self.apiUrl + endpoint,
+    auth: {
+      bearer: self.accessToken,
+    },
+  };
+  if (method === 'GET') {
+    opts.qs = params || {};
+    opts.json = true;
+  } else {
+    opts.json = params || true;
+  }
+  request(opts, function(err, http, response) {
+    if (err) {
+      return cb({
+        message: 'api_call_failed',
+        url: self.apiUrl + endpoint,
+        method: method,
+        details: e,
+      });
+    }
+    cb(null, response);
+  });
+};
+*/
   private _api = async (
     method?: string,
     endpoint?: string | number,
     params?: any
   ) => {
-    // !!!
-    try {
-      if (introspection) console.log('\n', '_api = async (...) => {');
-      const url: string = this._apiUrl + endpoint;
-      const opts: any = {
-        auth: {
-          bearer: this._accessToken,
-        },
-        method,
-        url,
-      };
-      if (method === 'GET') {
-        opts.qs = params || {};
-        opts.json = true;
-      } else {
-        opts.json = params || true;
-      }
-      return request(opts);
-    } catch (error) {
-      if (introspection) console.log('\n', error.message);
-      // throw error;
+    let computeParams: any = {};
+    if (typeof params !== 'undefined' && typeof params === 'object') {
+      computeParams = params;
     }
+    computeParams.bearer = this._accessToken;
+
+    const url: string = this._apiUrl + endpoint;
+    const config: axios.AxiosRequestConfig = {
+      params: computeParams,
+      method,
+      url,
+    };
+    //   if (method === 'GET') {
+    //     opts.params = params || {};
+    //   }
+    return axios.default(config);
+  };
+  /*
+Method that appends the set account to the API calls so all calls are made
+to that account. Chage self.account to change the account used
+Questrade.prototype._accountApi = function(method, endpoint, params, cb) {
+  if (!this.account)
+    return cb({
+      message: 'no_account_selected',
+    });
+  this._api(method, '/accounts/' + this.account + endpoint, params, cb);
+};
+*/
+  private _accountApi = async (method?: any, endpoint?: any, params?: any) => {
+    if (!this._account) {
+      throw new Error('no_account_selected');
+    }
+    return this._api(method, `/accounts/${this._account}${endpoint}`, params);
   };
 
-  // Method that appends the set account to the API calls so all calls
-  // are made to that account. Chage this.account to change the account used
-  private _accountApi = async (method?: any, endpoint?: any, params?: any) => {
-    // !!!
-    try {
-      if (introspection) console.log('\n', '_accountApi = (...) => {');
-      if (!this._account) {
-        throw new Error('no_account_selected');
-      }
-      return this._api(method, `/accounts/${this._account}${endpoint}`, params);
-    } catch (error) {
-      if (introspection) console.log('\n', error.message);
-      // throw error;
-    }
-  };
+  // console.log('_account', this._account);
+  // console.log('_apiVersion', this._apiVersion);
+  // console.log('_authUrl', this._authUrl);
+  // console.log('_keyDir', this._keyDir);
+  // console.log('_keyFile', this._keyFile);
+  // console.log('seedToken', seedToken);
+
+  // console.log('_getKeyFile():', this._getKeyFile());
+  // console.log('_accessToken:', this._accessToken);
+  // console.log('_apiServer:', this._apiServer);
+  // console.log('_apiUrl:', this._apiUrl);
+  // console.log('_refreshToken:', this._refreshToken);
+  // console.log('_apiServer:', this._apiServer);
+
+  // console.log('_loadKey:', this._loadKey());
+  // console.log('_accessToken:', this._accessToken);
+  // console.log('_apiServer:', this._apiServer);
+  // console.log('_apiUrl:', this._apiUrl);
+  // console.log('_refreshToken:', this._refreshToken);
+  // console.log('_apiServer:', this._apiServer);
+
+  // console.log('_refreshKey:', this._refreshKey());
+  // console.log('_accessToken:', this._accessToken);
+  // console.log('_apiServer:', this._apiServer);
+  // console.log('_apiUrl:', this._apiUrl);
+  // console.log('_refreshToken:', this._refreshToken);
+  // console.log('_apiServer:', this._apiServer);
+  // * Gets name of the file where the refreshToken is stored
+  // * Reads the refreshToken stored in the file (if it exist)
+  // * otherwise uses the seedToken
+  // * Synchronously create a new directory
+  // * Refreshed the tokem (aka Logs in) using the latest RefreshToken
+  // * (or the SeedToken if no previous saved file)
+  // *   emit('refresh', this._refreshToken);
+  // * Method that actually mades the GET/POST request to Questrade
+  // * Method that appends the set account to the API calls so all calls
+  // * are made to that account. Chage account to change the account used
+
+  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
 import { QuestradeClass } from '.';
 
 try {
-  const q = new QuestradeClass('QpzUOwBpsGHjd_CTJuriaQBnRBCgoN1_0');
+  const q = new QuestradeClass('AATHoMFONVWl6OnNB4SVgwM8t2M-5vhU0');
   q.search('aapl', null)
     .then(value => console.log(value))
     .catch(err => console.log('err', err));
