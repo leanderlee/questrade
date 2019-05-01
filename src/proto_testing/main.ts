da3056589f21e933f16468a34f48f3d7274c4ef8
Benjamin Vincent <luxcium@neb401.com>
2019-04-30 23:25:55 -0400

    + questrade-ts@0.4.1

--- src/proto_testing/main.ts
+++ src/proto_testing/main.ts
@@ -1,5 +1,5 @@
 /** @format */
 
 import { main } from './index';
-const seedToken = '6EB7fuQB9_xJepOpdw8TBj-JAMwACDfI0';
+const seedToken = 'lNwA6-R9oeepOAbgnajCRW6JvHODgkjM0';
 main(seedToken).catch(error => console.log('MAINERROR:', error));
